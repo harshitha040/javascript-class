92e3055905fa9c6c2fab9cc630bbc7d97319597a
harshitha040 <harshithatenali111@gmail.com>
2026-01-30 19:23:08 +0530

    alert, document to show o/p

--- day2.js
+++ day2.js
@@ -1,5 +1,11 @@
-name="harshitha"
-console.log(name);
-console.warn("This is a warning message");
-console.error("This is an error message");
-console.info("info");
+// name="harshitha"
+// console.log(name);
+// console.warn("This is a warning message");
+// console.error("This is an error message");
+// console.info("info");
+
+
+// document.write("yes");
+
+alert("be attentive in class")
+window.alert("i will ask questions")
