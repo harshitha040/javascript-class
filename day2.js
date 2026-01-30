4eac31b2b0adf9ff0129d51b1634197c0d202c43
harshitha040 <harshithatenali111@gmail.com>
2026-01-30 21:49:14 +0530

    var,let ,const

--- day2.js
+++ day2.js
@@ -7,5 +7,32 @@
 
 // document.write("yes");
 
-alert("be attentive in class")
-window.alert("i will ask questions")
+// alert("be attentive in class")
+// window.alert("i will ask questions")
+
+
+// variables are case sensitive
+/*Name ="harshitha"
+name="varun"
+console.log(Name);
+console.log(name);*/
+
+// var age =21;
+// console.log(age);
+// var age;
+// age=22;  /*redeclare */
+// console.log(age);
+// age=24;
+// console.log(age);  /*reassign */
+
+
+// let age=21;
+// console.log(age);
+// // let age;  /*redeclare not allowed*/
+// // age=22;  
+// age=24;
+// console.log(age);  /*reassign */
+
+const pi=3.14;
+console.log(pi);
+// pi=3.15;  /*reassign not allowed*/
