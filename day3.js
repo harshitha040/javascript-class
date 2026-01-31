e34189e9cb95e6b791ad978342cf92f88b9c6de9
harshitha040 <harshithatenali111@gmail.com>
2026-01-31 11:40:39 +0530

    non primitive array

---
+++ day3.js
@@ -0,0 +1,31 @@
+// var name=prompt("enter ur name");
+// console.log(name); 
+// document.write(name);
+
+// var name=prompt("enter ur name");
+// var age=prompt("enter ur age");
+// console.log(name);
+// document.write(name);
+// console.log(age);
+// document.write(age);
+
+var person =["harshitha",22,"developer"];
+console.log(person);  //[ 'harshitha', 22, 'developer' ]
+
+var person=[];
+console.log(person); // []
+
+var person=[];
+console.log(person); // []
+person[0]="harshitha";
+person[1]=22;
+person[2]="developer";
+console.log(person); // [ 'harshitha', 22, 'developer' ]
+console.log(person[0]); //harshitha
+
+
+var car=new Array();
+car[0]="Thar";
+car[1]=2000;
+car[2]="black";
+console.log(car); // [ 'Thar', 2000, 'black' ]
