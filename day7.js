c306e437248ce3ca4d48e684003d3b89c010e301
harshitha040 <harshithatenali111@gmail.com>
2026-02-02 20:20:25 +0530

    named and parameterized function

---
+++ day7.js
@@ -0,0 +1,14 @@
+/*function fun_name(){
+    var a=4;
+    var b=3;
+    var c=a+b;
+    console.log(c);
+    document.write(c);
+}
+fun_name(); //7 */ 
+
+
+function fun_name(a,b){;
+    console.log(a+b);
+}
+fun_name(5,6); // 11
