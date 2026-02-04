6bbb23ab067d7d803ec7360a8f080c6f6fc8694f
harshitha040 <harshithatenali111@gmail.com>
2026-02-04 16:29:34 +0530

    Template literals

---
+++ day11.js
@@ -0,0 +1,29 @@
+//basic interpolation
+var place = "Nellore"
+var food = "Biryani"
+console.log("harshi is from "+ place +" she like "+ food)
+console.log(`harshi is from ${place} she like ${food}`)
+
+//add expressions
+var num1=20;
+var num2=45;
+console.log("the addition of" + num1 + "and " + num2 +  "are" + (num1+num2))
+console.log(`the addition of ${num1}and ${num2} are ${num1+num2}`)
+
+//we can include the Additional quotes
+console.log(`harshish restuarnt is so famous for "biryani"`) //harshish restuarnt is so famous for "biryani"
+
+
+// we can write a multiple string 
+console.log("twinkle twinkle\n little start\n how i wonder what u are")
+// o/p : twinkle twinkle
+//        little start
+//        how i wonder what u are , here we use \n to get in seoerate line 
+
+console.log(`twinkle twinkle
+     little start
+      how i wonder what u are`) 
+// twinkle twinkle
+//        little start
+//        how i wonder what u are , here by using backticks we can write as above
+
