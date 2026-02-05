5c389863f2419fbcf0552b1fd8297d2f62b59db6
harshitha040 <harshithatenali111@gmail.com>
2026-02-05 14:53:03 +0530

    variable hoisting using var ,let, const

---
+++ day12.js
@@ -0,0 +1,32 @@
+/*console.log(food)
+var food="biryani"
+console.log(food) 
+//  o/p : undefined 
+//        biryani */
+
+/*
+var food;
+console.log(food)
+food = "mushroom biryani" 
+console.log(food)  // this is done in hoisting as u can see above 2 lines indicates as this
+*/
+
+
+/*
+console.log(name)
+let name ="harshi"
+
+it will display as below also
+let name; -- temporal dead zone
+conso;e.log(name)
+name = "harshi"; 
+*/
+
+/*
+console.log(chocolate)
+const chocolate = "5start" */
+
+/*
+const chocolate; --tdz
+console.log(chocolate);
+chocolate = "5start"  // this is how in backend , here we get reference error */ 
