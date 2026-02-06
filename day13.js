b596790d21838d50f25ced81362e3e7ade703436
harshitha040 <harshithatenali111@gmail.com>
2026-02-06 11:21:43 +0530

    math methods

---
+++ day13.js
@@ -0,0 +1,33 @@
+var number =-12;
+console.log(Math.abs(number)) //12
+
+var num= 24.2434;
+console.log(Math.round(num)) //24
+
+var num=24.56;
+console.log(Math.round(num))//25
+
+var num1=24.6434;
+console.log(Math.ceil(num1)) //25
+console.log(Math.floor(num1)) //24
+
+var num2=4.2;
+console.log(Math.ceil(num2))// 5
+
+var num3=4.2;
+console.log(Math.floor(num3)) //4
+
+var num4 =4.7;
+console.log(Math.floor(num4)) //4
+
+var num5=5673.98333;
+console.log(Math.trunc(num5))//5673
+
+console.log(Math.min(1,4,8,5)) //1
+console.log(Math.max(1,4,8,5)) //8
+
+console.log(Math.sqrt(25))//5
+
+console.log(Math.pow(2,4)) //16
+
+console.log(Math.random())
