17e53bb220ecbe7865d1bfe981f228b3325799f6
harshitha040 <harshithatenali111@gmail.com>
2026-02-07 16:12:40 +0530

    diff types of scopes

---
+++ day15.js
@@ -0,0 +1,53 @@
+/* global 
+var a=9;
+function fun_name(){
+    console.log(a); //value is accessed inside function
+    if(true){
+        console.log(a); //value accessed in block , within  function
+    }
+}
+console.log(a); //outside the function 
+fun_name();
+*/
+
+/* functional /local 
+function add(){
+    var name="harshitha"
+    console.log(`the girl name is name is ${name}`) // variable accessed inside function
+    if(20==20){
+        console.log(`the girl name is name is ${name}`)
+    }
+}
+var name1="varun"
+if(true){
+    //console.log(`the boy name is name is ${name}`) //raise error because name is declared in function cannot accessed outside
+    console.log(`the boy name is name is ${name1}`) //the boy name is name is varun 
+}
+//console.log(name) //raise error because name is declared in function cannot accessed outside
+add();
+*/
+
+
+/* Block 
+function num(){
+    if(true){
+        var name="barre"
+        console.log(name +" likes chicken")
+    }
+    console.log(name +" likes chicken") // it can be access outside block because we use var key word ,var is a block scope
+}
+//console.log(name +" likes chicken") // error , we cannot access outside function
+num();
+*/
+
+function parent(){
+    var a=10;
+    //console.log(a-b); //arise error , we cannot access child function
+    function child(){
+        var b =5;
+        var c =a+b;
+        console.log(c);
+    }
+    child()
+}
+parent();
